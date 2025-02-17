2bd9fea90fd2aad8f347f70c5047398e85c59bf0
Andrea Mariotti <andrea1997mariotti@gmail.com>
2025-02-17 23:27:34 +0100

    Added path to .env file

--- backend/config/db_config.py
+++ backend/config/db_config.py
@@ -1,6 +1,11 @@
 import os
 from dotenv import load_dotenv
 
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+ENV_PATH = os.path.join(BASE_DIR, ".env")
+
+load_dotenv(ENV_PATH)
+
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
