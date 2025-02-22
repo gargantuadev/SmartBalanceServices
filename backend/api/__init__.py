549d4ba76d15d8b24c589ed0835847a7389f45ae
Andrea Mariotti <andrea1997mariotti@gmail.com>
2025-02-22 19:14:13 +0100

    commented db logic for now

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -1,25 +1,25 @@
 # __init__.py
 from flask import Flask
 from flask_cors import CORS
-from flask_sqlalchemy import SQLAlchemy
+#from flask_sqlalchemy import SQLAlchemy
 
 from config.settings import DevelopmentConfig, ProductionConfig
 from config.db_config import DATABASE_URL
 from .categories import categories_bp
-from .db import db
+#from .db import db
 
 def create_app():
     app = Flask(__name__)
     CORS(app)  # Enable CORS
     app.config.from_object(DevelopmentConfig)
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
+    """app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     db.init_app(app)
 
     with app.app_context():
-        db.create_all()
+        db.create_all()"""
 
     app.register_blueprint(categories_bp, url_prefix='/categories')
     #app.register_blueprint(api_bp, url_prefix='/api')  # Register the API blueprint
--- backend/config/db_config.py
+++ backend/config/db_config.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 
+"""
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 ENV_PATH = os.path.join(BASE_DIR, ".env")
 
@@ -9,4 +10,4 @@ load_dotenv(ENV_PATH)
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 if not DATABASE_URL:
-    raise ValueError("DATABASE_URL is not set in the .env file!")
+    raise ValueError("DATABASE_URL is not set in the .env file!")"""
